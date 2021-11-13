3c3c6b47e9a8564eb051bba52e2d9ca5bda31590
Shreyansh263 <64355314+Shreyansh263@users.noreply.github.com>
2021-11-13 14:02:55 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,10 @@ const uniqueValidator=require('mongoose-unique-validator');
 
 const userSchema=mongoose.Schema({
     email : {type : String, required : true, unique : true},
-    password : {type : String, required : true, unique : true}
+    password : {type : String, required : true, unique : true},
+    fname : {type : String, required : true},
+    lname : {type : String, required : true},
+    about : {type : String, required : true}
 });
 
 userSchema.plugin(uniqueValidator);
