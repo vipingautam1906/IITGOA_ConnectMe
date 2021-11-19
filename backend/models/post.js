6ec92e741df3f14548941e08713c5a199a6f969b
Shreyansh263 <64355314+Shreyansh263@users.noreply.github.com>
2021-11-19 12:38:48 +0530

    Update post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,7 +4,14 @@ const postSchema=mongoose.Schema({
     title : {type : String, required : true},
     content : {type : String, required : true},
     imagePath : {type : String, required : true},
-    creator : {type : mongoose.Schema.Types.ObjectId, ref : "User", required : true}
+    creator : {type : mongoose.Schema.Types.ObjectId, ref : "User", required : true},
+    comments : [
+            {
+                userId : {type : String, required : true},
+                name : {type : String, required : true},
+                message : {type : String, required : true}
+            }
+        ]   
 });
 
 module.exports=mongoose.model('Post',postSchema);
